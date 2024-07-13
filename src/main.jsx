943e84b4dd1a4f290fd74ff891a8bcbf844fe657
AlmahdiAhmed <almahdiahmed22@gmail.com>
2024-07-13 12:57:46 +0400

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store.js";
+import Cart from "./CartItem.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    ),
+  },
+  {
+    path: "/cart",
+    element: (
+      <Provider store={store}>
+        <Cart />
+      </Provider>
+    ),
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+    {/* <Provider store={store}>
+      <App />
+    </Provider> */}
+  </React.StrictMode>
+);
